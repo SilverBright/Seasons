d467cdb8630fa3c872dc96ba17c49f3f18e596a6
github username <github email address>
2019-05-18 19:27:24 -0700

    delete constructor, refactor state, move fetch request to componentDidMount

--- src/index.js
+++ src/index.js
@@ -4,28 +4,15 @@ import ReactDOM from 'react-dom';
 // Class Component
 
 class App extends React.Component {
-  // Now that we have a class component we can include state
-  // We need to initialize our state object. To do that we use constructor
-  // calling the constructor function OVERRIDES React's basic built-in construction functionality
-  constructor(props) {
-    // we must then reference the parent function by using super(props) so that we still get React's constructor functionality
-    // not calling super(props) will render an error
-    super(props);
+  state = { lat: null, errorMessage: ''};
 
-    // this.state = { key: value }
-    // This is only time we do direct assignment to this.state
-    this.state = { lat: null, errorMessage: '' };
-
+  componentDidMount() {
+    // console.log('Component rendered to screen');
     window.navigator.geolocation.getCurrentPosition(
       // position is a callback function
-      position => {
-        // Update the state (updating) by using setState
-        this.setState({ lat: position.coords.latitude });
-      },
-      // second argument/callback
-      error => {
-        this.setState({ errorMessage: error.message });
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+       // Update the state (updating) by using setState
+      err => this.setState({ errorMessage: err.message })
     );
   }
 
@@ -40,10 +27,12 @@ class App extends React.Component {
       }
 
       return <div>loading...</div>;
-    };
+    }
   }
-
-
+    
+  ReactDOM.render(<App />,
+    document.querySelector('#root')
+  );
 
 // Functional Component
 
@@ -60,6 +49,3 @@ class App extends React.Component {
 //      <div>Latitude: </div>
 //   );
 
-ReactDOM.render(<App />,
-  document.querySelector('#root')
-);
