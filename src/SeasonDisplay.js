3143b586be35e8a21ffe0e99edb6dffc8f9c801f
github username <github email address>
2019-05-18 20:01:13 -0700

    add icons to SeasonDisplay

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,35 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
-  return <div>Season Display</div>
+const seasonConfig = {
+  summer: {
+    message: "Let's hit the beach!",
+    iconName: 'sun'
+  },
+  winter: {
+    message: "Burrr!!! It's cold!!",
+    iconName: 'snowflake'
+  }
+};
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat < 0 ? 'winter' : 'summer';
+  }
+};
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { message, iconName } = seasonConfig[season];
+
+  return (
+    <div>
+      <i className={`${iconName} icon`} />
+      <h1>{message}</h1>
+      <i className={`${iconName} icon`} />
+    </div>
+  )
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 // Class Component
 
@@ -24,7 +25,7 @@ class App extends React.Component {
       }
 
       if (this.state.lat) {
-        return <div>Latitude: {this.state.lat}</div>;
+        return <SeasonDisplay lat={this.state.lat}/>
       }
 
       return <div>loading...</div>;
