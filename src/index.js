ad9d906a2c822d20503cfa759ec80a1535dc8283
github username <github email address>
2019-05-18 18:34:32 -0700

    add constructor, state, and setState to index.js

--- src/index.js
+++ src/index.js
@@ -4,19 +4,38 @@ import ReactDOM from 'react-dom';
 // Class Component
 
 class App extends React.Component {
-  render() {
-    window.navigator.geolocation.getCurrentPosition(
-    // first argument - success callback
-    (position) => console.log(position),
-    // second argument
-    (error) => console.log(error)
-  );
+  // Now that we have a class component we can include state
+  // We need to initialize our state object. To do that we use constructor
+  // calling the constructor function OVERRIDES React's basic built-in construction functionality
+  constructor(props) {
+    // we must then reference the parent function by using super(props) so that we still get React's constructor functionality
+    // not calling super(props) will render an error
+    super(props);
 
-  return (
-    <div>Latitude: </div>
+    // this.state = { key: value }
+    // This is only time we do direct assignment to this.state
+    this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      // first argument - success callback
+      // Call setState to update from null to new state
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      // second argument
+      error => console.log(error)
     );
   }
-}
+
+  // Render method must always exist in every component
+  render() {
+    return (
+      <div>
+        Latitude: {this.state.lat}
+      </div>
+    );
+  }
+
 
 
 // Functional Component
@@ -33,9 +52,8 @@ class App extends React.Component {
 //   return (
 //      <div>Latitude: </div>
 //   );
-// }
+}
 
-ReactDOM.render(
-  <App />,
+ReactDOM.render(<App />,
   document.querySelector('#root')
 );
